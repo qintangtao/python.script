f00420dc3002781b236203f7247ea63a0ddcd172
qtt <qintangtao@apkshow.com>
2018-07-12 17:20:38 +0800

    fix button

--- shuqi/mainwindow.py
+++ shuqi/mainwindow.py
@@ -384,6 +384,7 @@ class MainWindow(QtGui.QWidget):
             self.ui.pushButton_before_page.setEnabled(False)
             self.ui.pushButton_after_page.setEnabled(False)
             self.ui.pushButton_refresh.setEnabled(False)
+            self.ui.pushButton_remove.setEnabled(False)
             self.ui.pushButton_sources.setEnabled(False)
             self.ui.pushButton_start.setEnabled(False)
             self.ui.pushButton_stop.setEnabled(True)
@@ -391,26 +392,8 @@ class MainWindow(QtGui.QWidget):
     def onStopClicked(self):
         for dump in self.listdump:
             dump.stop()
-        self.ui.comboBox_gender.setEnabled(True)
-        self.ui.comboBox_major.setEnabled(True)
-        self.ui.comboBox_minor.setEnabled(True)
-        self.ui.comboBox_status.setEnabled(True)
-        self.ui.comboBox_sort.setEnabled(True)
-        self.ui.lineEdit_page_index.setEnabled(True)
-        self.ui.pushButton_refresh.setEnabled(True)
-        self.ui.pushButton_sources.setEnabled(True)
-        self.ui.pushButton_start.setEnabled(True)
+
         self.ui.pushButton_stop.setEnabled(False)
-
-        if self.page_index > 0:
-            self.ui.pushButton_before_page.setEnabled(True)
-        else:
-            self.ui.pushButton_before_page.setEnabled(False)
-
-        if self.page_index + 1 < self.page_total:
-            self.ui.pushButton_after_page.setEnabled(True)
-        else:
-            self.ui.pushButton_after_page.setEnabled(False)
 
     def onSignalLog(self, index, log):
         self.model.setLog(index, log)
@@ -437,3 +420,30 @@ class MainWindow(QtGui.QWidget):
                 source = self.model.getSources(row)
                 self.sender().start(row, self.path, bid, self.uid,
                                     source['site'], source['site_name'])
+
+        else:
+            for dump in self.listdump:
+                if dump.isRunning():
+                    return
+
+            self.ui.comboBox_gender.setEnabled(True)
+            self.ui.comboBox_major.setEnabled(True)
+            self.ui.comboBox_minor.setEnabled(True)
+            self.ui.comboBox_status.setEnabled(True)
+            self.ui.comboBox_sort.setEnabled(True)
+            self.ui.lineEdit_page_index.setEnabled(True)
+            self.ui.pushButton_refresh.setEnabled(True)
+            self.ui.pushButton_remove.setEnabled(True)
+            self.ui.pushButton_sources.setEnabled(True)
+            self.ui.pushButton_start.setEnabled(True)
+            self.ui.pushButton_stop.setEnabled(False)
+
+            if self.page_index > 0:
+                self.ui.pushButton_before_page.setEnabled(True)
+            else:
+                self.ui.pushButton_before_page.setEnabled(False)
+
+            if self.page_index + 1 < self.page_total:
+                self.ui.pushButton_after_page.setEnabled(True)
+            else:
+                self.ui.pushButton_after_page.setEnabled(False)

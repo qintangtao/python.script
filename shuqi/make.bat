f1472de845ac6cee421a19ce82814f55156f1198
qtt <qintangtao@apkshow.com>
2018-07-19 10:15:19 +0800

    fix make.bat import self module

--- shuqi/make.bat
+++ shuqi/make.bat
@@ -1,3 +1,15 @@
+@echo off
+set "bd=%cd%"
+::echo %bd%
+cd ..
+set "bbd=%cd%"
+::echo %bbd%
+cd %bd%
+::echo %cd%
+if "%PYTHONPATH%" == "" ( 
+	set PYTHONPATH=%bbd%;%PYTHONPATH%
+)
+::echo %PYTHONPATH%
 pyrcc4 res.qrc -o res.py
 call pyuic4 mainwindow.ui -o ui_mainwindow.py
 pyinstaller -F -w -i bug.ico -n dump.exe --clean --key=shuqi123456 main.py

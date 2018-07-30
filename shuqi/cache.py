1bad1aa98b22c227c2dfdc197a06e2de71046958
Tato <qintangtao@qq.com>
2018-07-30 21:47:35 +0800

    del BaseCache

--- shuqi/cache.py
+++ shuqi/cache.py
@@ -3,38 +3,6 @@
 import os
 from qin.cache import BaseCache
 
-'''
-class BaseCache(object):
-
-    def __init__(self, path, key, iv):
-        self._path = path
-        self._prpcrypt = prpcrypt(key, iv)
-
-    def _encrypt(self, dict):
-        return self._prpcrypt.encrypt(json.dumps(dict))
-
-    def _decrypt(self, text):
-        return json.loads(self._prpcrypt.decrypt(text))
-
-    def write(self, dict):
-        try:
-            data = self._encrypt(dict)
-            return utils.save_file_w(self._path, data)
-        except Exception, e:
-            logging.error(str(e))
-        return False
-
-    def read(self):
-        try:
-            if os.path.exists(self._path):
-                data = utils.read_file_r(self._path)
-                if data is not None:
-                    return self._decrypt(data)
-        except Exception, e:
-            logging.error(str(e))
-        return None
-'''
-
 
 class SourcesCache(BaseCache):
 

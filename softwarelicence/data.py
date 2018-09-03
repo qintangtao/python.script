116aac2336cc88f871da70f8a5f714b612f32a2d
qtt <qintangtao@apkshow.com>
2018-09-03 09:53:50 +0800

    rm SoftwareLicence code

--- softwarelicence/data.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-public_key = '-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCpsOUR/BUIJvkuVn0Xuu/r4GLS\nLDw4rA7C/cF1XMfba7Wxly7z76uGFuLHDrYHmKYnYwc8XeVnV/2ur+Db7k3YGg78\nturtDQOGM1Cn1koY4mRXuT0PGaE5V++/5uczZYRt+YA6IgdYbeNn4rd1VZBSlcYd\nzspnkVfrZMYdB+NHcQIDAQAB\n-----END PUBLIC KEY-----'

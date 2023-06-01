a9a217bb3ef5de224cd477a1cc0f55ca2b54eb4c
Tangtao Qin <qintangtao@qq.com>
2023-06-01 15:46:06 +0800

    pyhon3

---
+++ qin3/db.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import logging
+import sqlite3
+
+
+class DbSqlite3(object):
+
+    def __init__(self, database):
+        self._open(database)
+
+    def __del__(self):
+        self._close()
+
+    def _open(self, database):
+        try:
+            self._conn = sqlite3.connect(database, check_same_thread=False)
+        except Exception as e:
+            logging.error(str(e))
+            self._conn = None
+
+    def _close(self):
+        if self._conn:
+            self._conn.close()
+            self._conn = None
+
+    def _executeDML(self, sql):
+        # INSERT、UPDATE、DELETE
+        if self._conn:
+            try:
+                c = self._conn.cursor()
+                c.execute(sql)
+                self._conn.commit()
+                return True
+            except Exception as e:
+                logging.error(str(e))
+        return False
+
+    def _executeDQL(self, sql):
+        # SELECT
+        if self._conn:
+            try:
+                c = self._conn.cursor()
+                return c.execute(sql)
+            except Exception as e:
+                logging.error(str(e))
+        return None
